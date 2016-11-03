6604625dc326265d17fdd105c0387aef82714da4
Tim Rijkse | Blocklevel <tim@blocklevel.nl>
2016-11-03 17:50:26 +0100

    Default styles for homepage

--- src/app/page/home/home.js
+++ src/app/page/home/home.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
+import styles from './home.scss'
 import template from './home.html'
 
 export default {
   template,
 
   mounted () {
-    console.log('Home mounted', this.styles)
+    console.log('Home mounted')
   }
 }
