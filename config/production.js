96cd1cfdd647e45f3ef9b8de73ea492fc7bd7816
Jonas Kuiler | Blocklevel <jonas@blocklevel.nl>
2016-11-03 12:14:29 +0100

    update production build file

--- config/production.js
+++ config/production.js
@@ -1,19 +1,17 @@
-var path = require('path');
-
 module.exports = {
-  // paths: {
-  //   index: path.resolve(__dirname, '../backend/resources/views/layouts/build.blade.php'),
-  //   assetsRoot: path.resolve(__dirname, '../htdocs'),
-  //   assetsPublicPath: '/'
-  // },
-  // settings: {
-  //   productionSourceMap: true,
-  //   productionGzip: false,
-  //   productionGzipExtensions: ['js', 'css']
-  // },
-  // definitions: {
-  //   API_URL: 'https://blocklevel.nl/api',
-  //   FACEBOOK_APP_KEY: 1478098066847,
-  //   FACEBOOK_APP_SECRET: 1478098066847
-  // }
+  paths: {
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    assetsRoot: '/asset'
+  },
+  settings: {
+    productionSourceMap: true,
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css']
+  },
+  definitions: {
+    API_URL: 'https://blocklevel.nl/api',
+    FACEBOOK_APP_KEY: 1478098066847,
+    FACEBOOK_APP_SECRET: 1478098066847
+  }
 };
