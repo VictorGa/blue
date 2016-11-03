ad493b9a5d4fe999218629f6cf91f6249363b506
Jonas Kuiler | Blocklevel <jonas@blocklevel.nl>
2016-11-03 10:06:05 +0100

    add basic build script

---
+++ webpack/build/build.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var {paths,settings} = require('../../config');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.build');
+var timestamp = Math.floor(new Date().getTime() / 1000);
+var assetRoot = `version/${timestamp}/${paths.assetsRoot}`;
+
+webpackConfig.output = {
+  path: paths.assetsRoot,
+  filename: `${assetRoot}/js/[name].js`,
+  chunkFilename: `${assetRoot}/js/[id].js`
+};
+
+webpack(webpackConfig, function (err, stats) {
+  // spinner.stop();
+  if (err) throw err;
+
+  process.stdout.write(stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }) + '\n')
+});
