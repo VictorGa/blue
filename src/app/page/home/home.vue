0527195c9a15b9461202cdb5aabfe828722741ec
Tim Rijkse | Blocklevel <tim@blocklevel.nl>
2016-11-07 13:57:44 +0100

    Add separate file for scss

--- src/app/page/home/home.vue
+++ src/app/page/home/home.vue
@@ -1,25 +1,8 @@
-<style lang="scss" module>
-.page {
-  flex: 1;
-  background: color('main');
-}
-
-.title {
-  display: inline-block;
-  padding: 20px;
-  background: green;
-
-  span {
-    float: left;
-    background: blue;
-  }
-}
-</style>
+<script src="./home.js"></script>
+<style lang="scss" module src="./home.scss"></style>
 
 <template>
   <div :class="$style.page">
     <h2 :class="$style.title">Home <span>page</span></h2>
   </div>
 </template>
-
-<script src="./home.js"></script>
