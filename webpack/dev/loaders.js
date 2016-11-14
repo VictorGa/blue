185da5cd20f2cf160c8fbc515b8b69576263b421
Matteo Gabriele <matteo@blocklevel.nl>
2016-11-14 12:56:06 +0100

    refactor(loaders) change loader composition style from string to query object
    closes #4

--- webpack/dev/loaders.js
+++ webpack/dev/loaders.js
@@ -1,4 +1,21 @@
+var combineLoaders = require('webpack-combine-loaders')
+
 module.exports = [{
   test: /\.css$/,
-  loader: 'style-loader!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader',
+  loader: combineLoaders([
+    {
+      loader: 'style-loader'
+    },
+    {
+      loader: 'css-loader',
+      query: {
+        modules: true,
+        importLoaders: 1,
+        localIdentName: '[name]__[local]___[hash:base64:5]'
+      }
+    },
+    {
+      loader: 'postcss-loader'
+    }
+  ])
 }];
