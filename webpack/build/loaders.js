9e91e2dde71973b71ab9787e12da55e1ea44021d
Jonas Kuiler <jonaskuiler@gmail.com>
2016-11-15 09:19:03 +0100

    seperate environment specific loaders and plugins

--- webpack/build/loaders.js
+++ webpack/build/loaders.js
@@ -1,6 +1,8 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = [{
-  test: /\.css$/,
-  loader: ExtractTextPlugin.extract('style-loader', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss')
-}];
+module.exports = [
+  {
+    test: /\.css$/,
+    loader: ExtractTextPlugin.extract('style-loader', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss')
+  }
+];
--- webpack/dev/loaders.js
+++ webpack/dev/loaders.js
@@ -1,4 +1,22 @@
-module.exports = [{
-  test: /\.css$/,
-  loader: 'style-loader!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader',
-}];
+module.exports = [
+  {
+    test: /\.css$/,
+    loader: 'style-loader!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader',
+  },
+  {
+    test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+    loader: 'url',
+    query: {
+      limit: 10000,
+      name: 'dist/image/[name].[hash:7].[ext]'
+    }
+  },
+  {
+    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    loader: 'url',
+    query: {
+      limit: 10000,
+      name: 'dist/font/[name].[hash:7].[ext]'
+    }
+  }
+];
