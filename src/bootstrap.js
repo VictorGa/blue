f670bb452787828c44435d139c3d498c85aaa45e
Jonas Kuiler <jonaskuiler@gmail.com>
2016-11-23 16:19:01 +0100

    feat(social) use vue-social

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import VueSocial from 'vue-social'
 import VueRouter from 'vue-router'
 import VueAnalytics from 'vue-analytics'
 import { sync } from 'vuex-router-sync'
@@ -41,6 +42,17 @@ sync(store, router)
  * Analytics
  */
 Vue.use(VueAnalytics, { router })
+
+/**
+ * Social
+ */
+Vue.use(VueSocial, {
+  // Provide this config when you want to use social login
+  // auth: {
+  //   linkedin: `${domain}/auth/linkedin`,
+  //   facebook: `${domain}/auth/facebook`
+  // }
+})
 
 /**
  * Bootstrap the application
